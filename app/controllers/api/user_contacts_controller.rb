63491d0b39abc4cbf4825d208bbf4d3d93e5d70c
arun61991 <arun.vaishnav@green-apex.com>
2021-01-04 16:38:51 +0530

    image added

--- app/controllers/api/user_contacts_controller.rb
+++ app/controllers/api/user_contacts_controller.rb
@@ -5,7 +5,64 @@ class Api::UserContactsController < ApplicationController
   # GET /user_contacts.json
   def index
     @user_contacts = UserContact.all
+    render :json => user_contact_data(@user_contacts)
   end
+
+
+  def user_contact_data user_contacts
+    data = []
+    user_contacts.each do |user_contact|
+      data << user_contact_json(user_contact)
+    end
+    data
+  end
+
+  def user_contact_json(user_contact)
+    {
+      id: user_contact.id,
+      name: user_contact.name,
+      email: user_contact.email,
+      address: user_contact.address,
+      gender: user_contact.gender,
+      profile_pic: user_contact.profile_pic,
+      phone: user_contact_phones(user_contact.phones),
+      products: user_contact_products(user_contact.products)
+    }
+  end
+
+  def user_contact_phones(phones)
+    data = []
+    phones.each do |phone|
+      data << user_contact_phone_json(phone)
+    end
+    data
+  end
+
+  def user_contact_phone_json(phone)
+    {
+      mobile: phone.mobile,
+      home: phone.home,
+      office: phone.office
+    }
+  end
+
+  def user_contact_products(products)
+    data = []
+    products.each do |product|
+      data << user_contact_product_json(product)
+    end
+    data
+  end
+
+  def user_contact_product_json(product)
+    {
+      product_id: product.id,
+      product_name: product.product_name,
+      product_image: product.product_image,
+      product_price: product.product_price
+    }
+  end
+
 
   # GET /user_contacts/1
   # GET /user_contacts/1.json
